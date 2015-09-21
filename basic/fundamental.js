76ab00f3658aa35d9605bf045a240c3554a7aedd
master <fedushov@mail.ru>
2015-09-22 01:31:36 +0600

    change copypast to eval feature

--- basic/fundamental.js
+++ basic/fundamental.js
@@ -1,5 +1,7 @@
 // JavaScript: ECMA + BOM + DOM
-// plog("",); - log to page
+// plog(""); - log to page
+
+var $eval = "eval";
 
 function data_types(){
     /**
@@ -22,27 +24,27 @@ function data_types(){
     '10'
     "10"
 
-    plog("disc C:\\ is full", "disc C:\ is full");
-    plog("disc C:\\\\ is full", "disc C:\\ is full");
-    plog("\\u2014 (unicode)", "\u2014");
-    plog("B\\x24F (hex)", "B\x24F");
-    plog("'hello' +' world' (concatenation)", "hello" + "wordl");
-    plog("'xyz' < 'abc' (compare strings perhaps, but not acceptable)", 'xyz' < 'abc');
-    plog("'a' > 'A' (compare numberic representation)", 'a' > 'A');
-    plog("'xyz' == 'abc'", 'xyz' == 'abc');
-    plog("'xyz' == 'xyz'", 'xyz' == 'xyz');
-    plog("'Xyz' == 'xyz'", 'Xyz' == 'xyz');
+    plog("disc C:\\ is full", ["disc C:\ is full"]);
+    plog("disc C:\\\\ is full", ["disc C:\\ is full"]);
+    plog("\\u2014", ["\u2014", "unicode"]);
+    plog("B\\x24F", ["B\x24F", "hex"]);
+    plog("'hello' + ' world!'", [$eval]);
+    plog("'xyz' < 'abc'", [$eval,"compare strings perhaps, but not acceptable"]);
+    plog("'a' > 'A'",[$eval,"compare numberic representation"]);
+    plog("'xyz' == 'abc'", [$eval]);
+    plog("'xyz' == 'xyz'", [$eval]);
+    plog("'Xyz' == 'xyz'", [$eval]);
 
     /**
      * Data types: Boolean:
      * */
 
     plog("data types: Boolean:")
-    plog("Boolean('')", Boolean(""));
-    plog("Boolean('false')", Boolean("false"));
-    plog("Boolean('anystring')", Boolean("anystring"));
-    plog("Boolean(0)", Boolean(0));
-    plog("Boolean(-1)", Boolean(-1));
+    plog("Boolean('')", [$eval]);
+    plog("Boolean('false')", [$eval]);
+    plog("Boolean('anystring')", [$eval]);
+    plog("Boolean(0)", [$eval]);
+    plog("Boolean(-1)",[$eval]);
 
 
 }
@@ -53,37 +55,37 @@ function operators(){
      **/
 
     plog("operators: + data types: Number:");
-    plog("2 + 2", 2 + 2);
-    plog("(2 + 2) * 2", (2 + 2) * 2);
-    plog("2 - 4", 2 - 4);
-    plog("2 * 2", 2 * 2);
-    plog("3 / 2", 3 / 2);
-    plog("3 % 2", 3 % 2);
-    plog("typeof(2 + 2)", typeof(2 + 2));
-    plog("typeof(3 / 2)", typeof(3 / 2));
+    plog("2 + 2", [$eval]);
+    plog("(2 + 2) * 2", [$eval]);
+    plog("2 - 4", [$eval]);
+    plog("2 * 2", [$eval]);
+    plog("3 / 2", [$eval]);
+    plog("3 % 2", [$eval]);
+    plog("typeof(2 + 2)", [$eval]);
+    plog("typeof(3 / 2)", [$eval]);
 
     plog("operators: + data types: Boolean:");
-    plog("1 > 2", 1 > 2);
-    plog("2 == 2", 2 == 2);
-    plog("2 == '2' (not compared types, cast type," +
-        "!NOTE in javascript String type has more priority than Number type)", 2 == '2');
-    plog("2 === 2", 2 === 2);
-    plog("2 === '2' (compare types)" , 2 === '2');
-    plog("2 != 1", 2 != 1);
-    plog("1 != '1'", 1 != '1');
+    plog("1 > 2", [$eval]);
+    plog("2 == 2", [$eval]);
+    plog("2 == '2'", [$eval, "not compared types, cast type," +
+        "!NOTE in javascript String type has more priority than Number type"]);
+    plog("2 === 2", [$eval]);
+    plog("2 === '2'" , [$eval, "compare types"]);
+    plog("2 != 1", [$eval]);
+    plog("1 != '1'", [$eval]);
 
     plog("operators:");
-    plog("2 && 3 (cast to Boolean type: true && true, compare and return last value," +
-        "were compiler was stopped)", 2 && 3);
-    plog("3 && 2", 3 && 2);
-    plog("0 && 2 (if checked value is false: false && true," +
-         "compiler stoped before &&, and return value were it was stoped)", 0 && 2);
-    plog("2 || 4", 2 || 4);
-    plog("'' || 0 || false || 4 (false || false || false || true)", "" || 0 || false || 4);
-    plog("!true", !true);
-    plog("!'1'", !"1");
-    plog("!'false' ('false' this is true)", !"false");
-    plog("1 && 2 || false && true ( && operator higher priority than || )" , 1 && 2 || false && true );
+    plog("2 && 3", [$eval, "cast to Boolean type: true && true, compare and return last value," +
+        "were compiler was stopped"]);
+    plog("3 && 2", [$eval]);
+    plog("0 && 2", [$eval, "if checked value is false: false && true," +
+        "compiler stoped before &&, and return value were it was stoped"]);
+    plog("2 || 4", [$eval]);
+    plog("'' || 0 || false || 4", [$eval,"false || false || false || true"]);
+    plog("!true", [$eval]);
+    plog("!'1'", [$eval]);
+    plog("!'false'",[$eval, "'false' - this is true"]);
+    plog("1 && 2 || false && true", [$eval, "&& operator higher priority than || "]);
 };
 
 function variables(){
@@ -112,5 +114,5 @@ function variables(){
 }
 
 variables();
-//data_types();
-//operators();
+data_types();
+operators();
--- common/log.js
+++ common/log.js
@@ -1,16 +1,24 @@
-function clog(description, result) {
-    console.log(description + " --> " + result)
+function clog(description) {
+    console.log(description)
 }
 
 function plog(description, result) {
     separator = " --> ";
     if(result === undefined){
-        result = "";
-        separator = "";
-        description =
+        $("#log").append(
             "<span style='margin-bottom: 0px; margin-top:0px;font-size: 20px;'>"
-                + description + "</span>"
+            + description + "</span>" + "</br>"
+        )
+        return;
     }
+    comment = "";
+    if(result[1] !== undefined )
+        comment = " (" + result[1] + ") ";
+    if(result[0] === 'eval')
+        result = eval(description);
+    else
+        result = result[0];
+    description += comment;
     $("#log").append(
         description + separator + result + "</br>"
     )
