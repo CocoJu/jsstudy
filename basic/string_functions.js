ecc4d048073ab0758181303a0a1373291e0496e8
master <fedushov@mail.ru>
2015-10-05 22:35:11 +0600

    refactoring substrCount function

--- basic/string_functions.js
+++ basic/string_functions.js
@@ -1,11 +1,17 @@
 function substrCount(str, findStr, offset, length){
+    if(offset === undefined)
+        var offset = 0;
+    if (length === undefined) {
+        str = str.slice(offset);
+    } else{
+        str = str.slice(offset, offset + length);
+    }
     var count = 0;
-    var pos = offset;
-    var maxPos = offset + length;
-    var lastPos = 0;
-    while((lastPos = str.indexOf(findStr, pos)) != -1 && pos < maxPos){
+    var pos = 0;
+    var lastPos;
+    while((lastPos = str.indexOf(findStr, pos)) != -1){
         count ++;
-        pos += lastPos + findStr.length +1;
+        pos = lastPos + findStr.length;
     }
     return count;
 }
--- common/controller.js
+++ common/controller.js
@@ -6,5 +6,5 @@
 //cycles();
 objects();
 
-testStr = "go ssdfjk skf jls a go go aaoo";
-clog(substrCount(testStr, "go", 3, 24));
+testStr = "gogogo_go";
+clog(substrCount(testStr, "go", 2 , 8));
